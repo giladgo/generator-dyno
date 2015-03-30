402f22cefb681da7605b774030a5c72fc88c18de
Dominik Schmidt <visualcake@me.com>
2015-03-30 21:17:37 +0200

    ask for project details using prompts

--- app/index.js
+++ app/index.js
@@ -24,8 +24,35 @@ Dyno.prototype.welcome = function welcome () {
   }
 };
 
+Dyno.prototype.askForProjectDetails = function askForProjectDetails () {
+  var done = this.async();
+  var _ = this._;
 
+  this.prompt([{
+    type: 'input',
+    name: 'name',
+    message: 'Please tell me how your project wants to be called.',
+    default: this.appname,
+    filter: function (val) {
+      return _.camelize(_.slugify(_.humanize(val)));
     }
+  }, {
+    type: 'input',
+    name: 'version',
+    message: 'What\'s the version of this neat project?',
+    default: '0.0.1',
+    validate: function (value) {
+      if (value == semver.valid(value)) {
+        return true;
+      } else {
+        return "Please use a semantic version number (http://semver.org/)"
+      }
+    }
+  }], function (opts) {
+    this.opts = opts;
+    done();
+  }.bind(this))
+}
 
 Dyno.prototype.askForPrecompiler = function askForPrecompiler () {
 
